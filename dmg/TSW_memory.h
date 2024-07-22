d2293da5c3db37ee8f07b03fa8b8d306371d29ba
Z-H-Sun <cs.en.ccme@gmail.com>
2024-07-22 00:09:47 -0400

    format code for better IDE traceback

---
+++ dmg/TSW_memory.h
@@ -0,0 +1,127 @@
+// aliases (a trick from https://stackoverflow.com/a/17615531/11979352)
+// do not use the linked win32 api if already loaded in TSW.exe
+#define LoadLibraryA __LoadLibraryA
+#define GetProcAddress __GetProcAddress
+#define MessageBoxA __MessageBoxA
+#define DrawTextA __DrawTextA
+#define BitBlt __BitBlt
+#define CreateCompatibleBitmap __CreateCompatibleBitmap
+#define CreateCompatibleDC __CreateCompatibleDC
+#define CreateFontIndirectA __CreateFontIndirectA
+#define CreatePenIndirect __CreatePenIndirect
+#define DeleteObject __DeleteObject
+#define SelectObject __SelectObject
+#define SaveDC __SaveDC
+#define RestoreDC __RestoreDC
+#define DeleteDC __DeleteDC
+#define SetBkMode __SetBkMode
+#define SetROP2 __SetROP2
+#define SetTextColor __SetTextColor
+#define TextOutA __TextOutA
+
+#define WINVER 0x0500 // minimum windows 2000
+#include <windows.h>
+
+// Delphi uses "Borland register" calling convention (argv passed to eax/edx/ecx)
+// so each extern function bears with __attribute__((__regparm__(3)))
+// https://en.wikipedia.org/wiki/X86_calling_conventions#Borland_register
+#define REGCALL __attribute__((__regparm__(3)))
+
+#define TTSW10_ADDR 0x48C510
+#define TTSW10_HERO_ITEM_ADDR 0x4B86C4
+#define TTSW10_HERO_STATUS_ADDR 0x4B8688
+#define TTSW10_ENEMY_STATUS_ADDR 0x489910
+#define TTSW10_MAP_STATUS_ADDR 0x4B8934
+#define TTSW10_STATUS_FACTOR_ADDR 0x4B8904
+#define TTSW10_GAMEMAP_BITMAP_1_ADDR 0x48C514
+#define TTSW10_GAMEMAP_BITMAP_2_ADDR 0x48C518
+#define TTSW10_GAMEMAP_LEFT_ADDR 0x48C578
+#define TTSW10_GAMEMAP_TOP_ADDR 0x48C57C
+#define TTSW10_EVENT_COUNT_ADDR 0x48C5AC
+
+#define TTSW10_IMAGE6_OFFSET 0x254
+#define TCONTROL_WIDTH_OFFSET 0x2C
+#define TCONTROL_HWND_OFFSET 0xC0
+#define TFORM_TCANVAS_OFFSET 0x120
+
+typedef struct {
+    INT32 HP;
+    INT32 ATK;
+    INT32 DEF;
+    INT32 GOLD;
+} HADG;
+typedef struct {
+    HADG HADG;
+    INT32 floor; INT32 maxFloor; INT32 x; INT32 y;
+    INT32 ylKey; INT32 redKey; INT32 blueKey; INT32 altarLv;
+} STATUS;
+typedef struct {
+    INT32 swordLv; INT32 shieldLv;
+    BOOL orbHero; BOOL orbWisdom; BOOL orbFlight;
+    BOOL cross; BOOL elixir; BOOL mattock;
+    BOOL desBall; BOOL warpWing; BOOL upWing;
+    BOOL loWing; BOOL dragonSl; BOOL snowXtal;
+    BOOL magicKey; BOOL supMatk; BOOL luckGold;
+} ITEM;
+
+// delphi system functions defined in TSW.exe
+typedef HDC REGCALL _TCanvas_GetHandle(HANDLE hTCanvas);
+typedef HANDLE REGCALL _TBitmap_GetCanvas(HANDLE hTBitmap);
+_TCanvas_GetHandle* TCanvas_GetHandle = (_TCanvas_GetHandle*)0x41A950;
+_TBitmap_GetCanvas* TBitmap_GetCanvas = (_TBitmap_GetCanvas*)0x41DAD8;
+
+// win32api functions already loaded in TSW.exe
+#undef LoadLibraryA
+#undef GetProcAddress
+#undef MessageBoxA
+#undef DrawTextA
+#undef BitBlt
+#undef CreateCompatibleBitmap
+#undef CreateCompatibleDC
+#undef CreateFontIndirectA
+#undef CreatePenIndirect
+#undef DeleteObject
+#undef SelectObject
+#undef SaveDC
+#undef RestoreDC
+#undef DeleteDC
+#undef SetBkMode
+#undef SetROP2
+#undef SetTextColor
+#undef TextOutA
+typedef HMODULE WINAPI _LoadLibraryA(LPCSTR lpLibFileName);
+typedef FARPROC WINAPI _GetProcAddress(HMODULE hModule, LPCSTR lpProcName);
+typedef int WINAPI _MessageBoxA(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType);
+typedef int WINAPI _DrawTextA(HDC hdc,LPCSTR lpchText,int cchText,LPRECT lprc,UINT format);
+typedef WINBOOL WINAPI _BitBlt(HDC hdc,int x,int y,int cx,int cy,HDC hdcSrc,int x1,int y1,DWORD rop);
+typedef HBITMAP WINAPI _CreateCompatibleBitmap(HDC hdc,int cx,int cy);
+typedef HDC WINAPI _CreateCompatibleDC(HDC hdc);
+typedef HFONT WINAPI _CreateFontIndirectA(CONST LOGFONTA *lplf);
+typedef HPEN WINAPI _CreatePenIndirect(CONST LOGPEN *plpen);
+typedef WINBOOL WINAPI _DeleteObject(HGDIOBJ ho);
+typedef HGDIOBJ WINAPI _SelectObject(HDC hdc,HGDIOBJ h);
+typedef int WINAPI _SaveDC(HDC hdc);
+typedef WINBOOL WINAPI _RestoreDC(HDC hdc,int nSavedDC);
+typedef WINBOOL WINAPI _DeleteDC(HDC hdc);
+typedef int WINAPI _SetBkMode(HDC hdc,int mode);
+typedef int WINAPI _SetROP2(HDC hdc,int rop2);
+typedef COLORREF WINAPI _SetTextColor(HDC hdc,COLORREF color);
+typedef WINBOOL WINAPI _TextOutA(HDC hdc,int x,int y,LPCSTR lpString,int c);
+_LoadLibraryA* LoadLibraryA = (_LoadLibraryA*)0x404BFC;
+_GetProcAddress* GetProcAddress = (_GetProcAddress*)0x404B84;
+_MessageBoxA* MessageBoxA = (_MessageBoxA*)0x401260;
+_DrawTextA* DrawTextA = (_DrawTextA*)0x404F0C;
+_BitBlt* BitBlt = (_BitBlt*)0x404C5C;
+_CreateCompatibleBitmap* CreateCompatibleBitmap = (_CreateCompatibleBitmap*)0x404C7C;
+_CreateCompatibleDC* CreateCompatibleDC = (_CreateCompatibleDC*)0x404C84;
+_CreateFontIndirectA* CreateFontIndirectA = (_CreateFontIndirectA*)0x404C94;
+_CreatePenIndirect* CreatePenIndirect = (_CreatePenIndirect*)0x404CA4;
+_DeleteObject* DeleteObject = (_DeleteObject*)0x404CCC;
+_SelectObject* SelectObject = (_SelectObject*)0x404DCC;
+_SaveDC* SaveDC = (_SaveDC*)0x404DC4;
+_RestoreDC* RestoreDC = (_RestoreDC*)0x404DBC;
+_DeleteDC* DeleteDC = (_DeleteDC*)0x404CBC;
+_SetBkMode* SetBkMode = (_SetBkMode*)0x404DE4;
+_SetROP2* SetROP2 = (_SetROP2*)0x404DF4;
+_SetTextColor* SetTextColor = (_SetTextColor*)0x404E04;
+_TextOutA* TextOutA = (_TextOutA*)0x404E34;
--- dmg/dmg.dll.c
+++ dmg/dmg.dll.c
@@ -1,72 +1,14 @@
-// Delphi uses "Borland register" calling convention (argv passed to eax/edx/ecx), so each extern function bears with __attribute__((__regparm__(3))): https://en.wikipedia.org/wiki/X86_calling_conventions#Borland_register
 // It is both more human-readable and more efficient to use `push` than `mov esp...`: https://stackoverflow.com/a/4535996; therefore the following 3 "-m..." options were added while compiling: https://stackoverflow.com/a/24982391
 // Likewise, no need to align stack on 16-byte boundaries (because this is not Linux; Windows use 4-byte boundaries instead), so added the 4-th "-m..." option: https://stackoverflow.com/a/43597693/11979352
 // Likewise, no need for procedure prolog and epilog (push ebp; mov ebp, esp; ...; leave), so added the following "-f..." option: https://stackoverflow.com/a/21620940/11979352
 // You MUST use a 32-bit compiler because only 32-bit DLL can be loaded by 32-bit TSW exe
 // gcc -std=gnu99 -Os -s -DNDEBUG -shared -Wl,--enable-auto-image-base,--enable-auto-import -mpush-args -mno-accumulate-outgoing-args -mno-stack-arg-probe -mpreferred-stack-boundary=2 -fomit-frame-pointer dmg.dll.def dmg.dll.c -o dmg.dll -lgdi32 -lmsimg32
 
-#define WINVER 0x0500 // minimum windows 2000
-#include <windows.h>
+#include "TSW_memory.h"
+#define NOINLINE __attribute__((noinline))
+#define get_p(p) *(DWORD*)(p) // get the DWORD value from the given pointer in memory
+#define get_h(p) *(HANDLE*)(p) // same as above; but the type is instead HANDLE
 
-#define TTSW10_ADDR 0x48C510
-#define TTSW10_HERO_ITEM_ADDR 0x4B86C4
-#define TTSW10_HERO_STATUS_ADDR 0x4B8688
-#define TTSW10_ENEMY_STATUS_ADDR 0x489910
-#define TTSW10_MAP_STATUS_ADDR 0x4B8934
-#define TTSW10_STATUS_FACTOR_ADDR 0x4B8904
-#define TTSW10_GAMEMAP_BITMAP_1_ADDR 0x48C514
-#define TTSW10_GAMEMAP_BITMAP_2_ADDR 0x48C518
-#define TTSW10_GAMEMAP_LEFT_ADDR 0x48C578
-#define TTSW10_GAMEMAP_TOP_ADDR 0x48C57C
-#define TTSW10_EVENT_COUNT_ADDR 0x48C5AC
-#define TTSW10_IMAGE6_OFFSET 0x254
-#define TCONTROL_WIDTH_OFFSET 0x2C
-#define TCONTROL_HWND_OFFSET 0xC0
-#define TFORM_TCANVAS_OFFSET 0x120
-#define REGCALL __attribute__((__regparm__(3)))
-typedef struct {
-    INT32 HP;
-    INT32 ATK;
-    INT32 DEF;
-    INT32 GOLD;
-} HADG;
-typedef struct {
-    HADG HADG;
-    INT32 floor; INT32 maxFloor; INT32 x; INT32 y;
-    INT32 ylKey; INT32 redKey; INT32 blueKey; INT32 altarLv;
-} STATUS;
-typedef struct {
-    INT32 swordLv; INT32 shieldLv;
-    BOOL orbHero; BOOL orbWisdom; BOOL orbFlight;
-    BOOL cross; BOOL elixir; BOOL mattock;
-    BOOL desBall; BOOL warpWing; BOOL upWing;
-    BOOL loWing; BOOL dragonSl; BOOL snowXtal;
-    BOOL magicKey; BOOL supMatk; BOOL luckGold;
-} ITEM;
-typedef DWORD REGCALL func0(void);
-typedef DWORD REGCALL func1(DWORD);
-typedef DWORD REGCALL func2(DWORD, DWORD);
-typedef DWORD REGCALL func3(DWORD, DWORD, DWORD);
-func1* TCanvas_GetHandle = (func1*)0x41A950;
-func1* TBitmap_GetCanvas = (func1*)0x41DAD8;
-FARPROC WINAPI Kernel32_LoadLibraryA = (FARPROC)0x404BFC;
-FARPROC WINAPI Kernel32_GetProcAddress = (FARPROC)0x404B84;
-FARPROC WINAPI User32_MessageBoxA = (FARPROC)0x401260;
-FARPROC WINAPI User32_DrawTextA = (FARPROC)0x404F0C;
-FARPROC WINAPI Gdi32_BitBlt = (FARPROC)0x404C5C;
-FARPROC WINAPI Gdi32_CreateCompatibleBitmap = (FARPROC)0x404C7C;
-FARPROC WINAPI Gdi32_CreateCompatibleDC = (FARPROC)0x404C84;
-FARPROC WINAPI Gdi32_CreateFontIndirect = (FARPROC)0x404C94;
-FARPROC WINAPI Gdi32_CreatePenIndirect = (FARPROC)0x404CA4;
-FARPROC WINAPI Gdi32_DeleteObject = (FARPROC)0x404CCC;
-FARPROC WINAPI Gdi32_SelectObject = (FARPROC)0x404DCC;
-FARPROC WINAPI Gdi32_SaveDC = (FARPROC)0x404DC4;
-FARPROC WINAPI Gdi32_RestoreDC = (FARPROC)0x404DBC;
-FARPROC WINAPI Gdi32_DeleteDC = (FARPROC)0x404CBC;
-FARPROC WINAPI Gdi32_SetBkMode = (FARPROC)0x404DE4;
-FARPROC WINAPI Gdi32_SetROP2 = (FARPROC)0x404DF4;
-FARPROC WINAPI Gdi32_SetTextColor = (FARPROC)0x404E04;
-FARPROC WINAPI Gdi32_TextOut = (FARPROC)0x404E34;
 const COLORREF color_OK = RGB(0x22, 0xAA, 0x22);
 const COLORREF color_suspicious = RGB(0xC0, 0xA0, 0x60);
 const COLORREF color_no_go = RGB(0xFF, 0x22, 0x22);
@@ -81,16 +23,16 @@ HPEN hPen_stroke, hPen_polyline; HFONT hFont_dmg;
 HDC hMemDC; HBITMAP hMemBmp;
 
 #define msgboxDWORD10(h,i) msgboxDWORD(h,i,10) // use 10-base
-static void REGCALL msgboxDWORD(HANDLE TTSW10, DWORD i, int base) { // for debug use; show value of int `i`
+NOINLINE static void REGCALL msgboxDWORD(HANDLE TTSW10, DWORD i, int base) { // for debug use; show value of int `i`
     HWND TTSW10_hWnd = *(HWND*)((DWORD)TTSW10+TCONTROL_HWND_OFFSET);
     char st[10];
     itoa(i, st, base);
-    User32_MessageBoxA(TTSW10_hWnd, st, "Debug Output", MB_ICONINFORMATION | MB_SETFOREGROUND);
+    MessageBoxA(TTSW10_hWnd, st, "Debug Output", MB_ICONINFORMATION | MB_SETFOREGROUND);
 }
 
-static int REGCALL itoa2(WORD i, char* a){ // return value is `len`; output string is (char*)a
+NOINLINE static int REGCALL itoa2(WORD i, char* a){ // return value is `len`; output string is (char*)a
     if (i == 0xFFFF) { // infinity
-        *(DWORD*)a = *(DWORD*)"???"; // set first four bytes
+        get_p(a) = get_p("???"); // set first four bytes
         return 3;
     }
     char len, digit;
@@ -107,7 +49,7 @@ static int REGCALL itoa2(WORD i, char* a){ // return value is `len`; output stri
     return (int)len;
 }
 
-static char REGCALL getMonsterID(UCHAR tileID) { // tileID -> monsterID
+NOINLINE static char REGCALL getMonsterID(UCHAR tileID) { // tileID -> monsterID
     if (tileID < 61)
         return -1; // 1-60: not a monster tile
     else if (tileID < 97)
@@ -124,12 +66,12 @@ static char REGCALL getMonsterID(UCHAR tileID) { // tileID -> monsterID
 }
 
 #define norm44(x,f) (x-1)/f+1 // this will divide x by 44 and ceil [NOTE: to minimize the risk of INT32 overflow of `x` (though unlikely), this is unsigned division, so need to rule out the case when x==0]
-static DWORD REGCALL getMonsterDmgCri(char monsterID) { // HIWORD=cri (0-32766; 0x7FFF=no show); LOWORD=dmg (0-65534; 0xFFFF=???); if the most significant bit is 1 (i.e., cri < 0), it means that the dmg is greater or equal to hero's HP
+NOINLINE static DWORD REGCALL getMonsterDmgCri(char monsterID) { // HIWORD=cri (0-32766; 0x7FFF=no show); LOWORD=dmg (0-65534; 0xFFFF=???); if the most significant bit is 1 (i.e., cri < 0), it means that the dmg is greater or equal to hero's HP
     ITEM TSW_hero_items = *(ITEM*)TTSW10_HERO_ITEM_ADDR;
     HADG TSW_hero_HADG = *(HADG*)TTSW10_HERO_STATUS_ADDR;
     HADG* TSW_enemy_HADG = (HADG*)TTSW10_ENEMY_STATUS_ADDR; // 33 monsters
     BOOL cross = TSW_hero_items.cross, dragonSl = TSW_hero_items.dragonSl;
-    UINT factor = *(UINT*)TTSW10_STATUS_FACTOR_ADDR; // 0 or 43 (back side)
+    UINT factor = get_p(TTSW10_STATUS_FACTOR_ADDR); // 0 or 43 (back side)
     int hHP = TSW_hero_HADG.HP, hATK = TSW_hero_HADG.ATK, hDEF = TSW_hero_HADG.DEF;
     int mHP = TSW_enemy_HADG[monsterID].HP, mATK = TSW_enemy_HADG[monsterID].ATK, mDEF = TSW_enemy_HADG[monsterID].DEF;
     if (factor) {
@@ -185,14 +127,14 @@ static DWORD REGCALL getMonsterDmgCri(char monsterID) { // HIWORD=cri (0-32766; 
     return dmg | (cri << 16); // MAKELONG(dmg, cri)
 }
 
-static void REGCALL draw_dmg(HANDLE TTSW10) {
-    DWORD TSW_tileSize = *(DWORD*)(*(DWORD*)((DWORD)TTSW10+TTSW10_IMAGE6_OFFSET)+TCONTROL_WIDTH_OFFSET);
+NOINLINE static void REGCALL draw_dmg(HANDLE TTSW10) {
+    DWORD TSW_tileSize = get_p(get_p((DWORD)TTSW10+TTSW10_IMAGE6_OFFSET)+TCONTROL_WIDTH_OFFSET);
     STATUS TSW_hero_status = *(STATUS*)TTSW10_HERO_STATUS_ADDR;
     WORD offset = 123u*(UCHAR)TSW_hero_status.floor + 2u;
     char* TSW_curFloor_tiles = (char*)TTSW10_MAP_STATUS_ADDR+offset;
     char strInt_1[8]; int lenInt_1;
     char strInt_2[8]; int lenInt_2;
-    Gdi32_BitBlt(hMemDC, 0, 0, 440, 440, NULL, 0, 0, WHITENESS);
+    BitBlt(hMemDC, 0, 0, 440, 440, NULL, 0, 0, WHITENESS);
     for (UCHAR i = 0; i < 121; i++) {
         char mID = getMonsterID(TSW_curFloor_tiles[i]);
         if (mID == -1)
@@ -203,80 +145,80 @@ static void REGCALL draw_dmg(HANDLE TTSW10) {
         WORD dmg = LOWORD(dmgCri), cri=HIWORD(dmgCri) & 0x7FFF;
 
         if ((INT32)dmgCri < 0) { // most significant bit set; inadequate HP
-            Gdi32_SetTextColor(hMemDC, color_no_go);
-            Gdi32_SetROP2(hMemDC, R2_BLACK);
+            SetTextColor(hMemDC, color_no_go);
+            SetROP2(hMemDC, R2_BLACK);
         }
         else {
-            Gdi32_SetTextColor(hMemDC, color_foreground);
-            Gdi32_SetROP2(hMemDC, R2_COPYPEN);
+            SetTextColor(hMemDC, color_foreground);
+            SetROP2(hMemDC, R2_COPYPEN);
         }
         BeginPath(hMemDC); // TODO:
         lenInt_1 = itoa2(dmg, strInt_1);
-        Gdi32_TextOut(hMemDC, x, y, strInt_1, lenInt_1);
+        TextOutA(hMemDC, x, y, strInt_1, lenInt_1);
         if (cri != 0x7FFF) {
             lenInt_2 = itoa2(cri, strInt_2);
-            Gdi32_TextOut(hMemDC, x, y-12, strInt_2, lenInt_2);
+            TextOutA(hMemDC, x, y-12, strInt_2, lenInt_2);
         }
         EndPath(hMemDC); // TODO:
         StrokePath(hMemDC); // TODO:
-        Gdi32_TextOut(hMemDC, x, y, strInt_1, lenInt_1);
+        TextOutA(hMemDC, x, y, strInt_1, lenInt_1);
         if (cri != 0x7FFF) {
-            Gdi32_TextOut(hMemDC, x, y-12, strInt_2, lenInt_2);
+            TextOutA(hMemDC, x, y-12, strInt_2, lenInt_2);
         }
     }
 }
 
-static void REGCALL overlay(HANDLE TTSW10) {
-    DWORD TSW_tileSize = *(DWORD*)(*(DWORD*)((DWORD)TTSW10+TTSW10_IMAGE6_OFFSET)+TCONTROL_WIDTH_OFFSET);
+NOINLINE static void REGCALL overlay(HANDLE TTSW10) {
+    DWORD TSW_tileSize = get_p(get_p((DWORD)TTSW10+TTSW10_IMAGE6_OFFSET)+TCONTROL_WIDTH_OFFSET);
     WORD TSW_mapSize = 11u * (UCHAR)TSW_tileSize;
-    DWORD TSW_mapLeft = *(DWORD*)(TTSW10_GAMEMAP_LEFT_ADDR), TSW_mapTop = *(DWORD*)(TTSW10_GAMEMAP_TOP_ADDR);
+    DWORD TSW_mapLeft = get_p(TTSW10_GAMEMAP_LEFT_ADDR), TSW_mapTop = get_p(TTSW10_GAMEMAP_TOP_ADDR);
 
-    HANDLE TTSW10_TCanvas = *(HANDLE*)((DWORD)TTSW10+TFORM_TCANVAS_OFFSET);
-    HDC TTSW10_TCanvas_hDC = (HDC)TCanvas_GetHandle((DWORD)TTSW10_TCanvas);
+    HANDLE TTSW10_TCanvas = get_h((DWORD)TTSW10+TFORM_TCANVAS_OFFSET);
+    HDC TTSW10_TCanvas_hDC = TCanvas_GetHandle(TTSW10_TCanvas);
     TransparentBlt(TTSW10_TCanvas_hDC, TSW_mapLeft, TSW_mapTop, TSW_mapSize, TSW_mapSize, hMemDC, 0, 0, TSW_mapSize, TSW_mapSize, RGB(0xFF,0xFF,0xFF)); // For some unknown reason, the black color can't be used as the mask color on some computers (what the heck?), see https://stackoverflow.com/q/70062625/11979352
 }
 
 extern void ini(void) { // initialize
-    HANDLE TTSW10 = *(HANDLE*)TTSW10_ADDR;
+    HANDLE TTSW10 = get_h(TTSW10_ADDR);
     LOGPEN lpen = {PS_SOLID, {3, 0}, color_background};
-    hPen_stroke = (HPEN)Gdi32_CreatePenIndirect(&lpen);
+    hPen_stroke = CreatePenIndirect(&lpen);
     lpen.lopnColor = color_polyline;
-    hPen_polyline = (HPEN)Gdi32_CreatePenIndirect(&lpen);
-    hFont_dmg = (HFONT)Gdi32_CreateFontIndirect(&lfont_dmg);
+    hPen_polyline = CreatePenIndirect(&lpen);
+    hFont_dmg = CreateFontIndirectA(&lfont_dmg);
 
-    HANDLE TTSW10_TCanvas = *(HANDLE*)((DWORD)TTSW10+TFORM_TCANVAS_OFFSET);
-    HDC TTSW10_TCanvas_hDC = (HDC)TCanvas_GetHandle((DWORD)TTSW10_TCanvas);
-    hMemDC = (HDC)Gdi32_CreateCompatibleDC(TTSW10_TCanvas_hDC);
-    hMemBmp = (HBITMAP)Gdi32_CreateCompatibleBitmap(TTSW10_TCanvas_hDC, 440, 440);
-    Gdi32_SetBkMode(hMemDC, TRANSPARENT);
-    Gdi32_SaveDC(hMemDC); // see `RestoreDC` in `finalize`
-    Gdi32_SelectObject(hMemDC, hMemBmp);
-    Gdi32_SelectObject(hMemDC, hPen_stroke);
-    Gdi32_SelectObject(hMemDC, hFont_dmg);
+    HANDLE TTSW10_TCanvas = get_h((DWORD)TTSW10+TFORM_TCANVAS_OFFSET);
+    HDC TTSW10_TCanvas_hDC = TCanvas_GetHandle(TTSW10_TCanvas);
+    hMemDC = CreateCompatibleDC(TTSW10_TCanvas_hDC);
+    hMemBmp = CreateCompatibleBitmap(TTSW10_TCanvas_hDC, 440, 440);
+    SetBkMode(hMemDC, TRANSPARENT);
+    SaveDC(hMemDC); // see `RestoreDC` in `finalize`
+    SelectObject(hMemDC, hMemBmp);
+    SelectObject(hMemDC, hPen_stroke);
+    SelectObject(hMemDC, hFont_dmg);
 
     //RECT rect = {180, 30, 620, 440};
-    //User32_DrawTextA(TTSW10_TCanvas_hDC, "Golden Knight is Tsundere!\nSays Zeno.", -1, &rect, DT_CENTER);
+    //DrawTextA(TTSW10_TCanvas_hDC, "Golden Knight is Tsundere!\nSays Zeno.", -1, &rect, DT_CENTER);
 }
 extern void fin(void) { // finalize
-    Gdi32_RestoreDC(hMemDC, -1); // this will deselect custom GDI objects so they can be properly disposed of
-    Gdi32_DeleteDC(hMemDC);
-    Gdi32_DeleteObject(hMemBmp);
-    Gdi32_DeleteObject(hPen_stroke);
-    Gdi32_DeleteObject(hPen_polyline);
-    Gdi32_DeleteObject(hFont_dmg);
+    RestoreDC(hMemDC, -1); // this will deselect custom GDI objects so they can be properly disposed of
+    DeleteDC(hMemDC);
+    DeleteObject(hMemBmp);
+    DeleteObject(hPen_stroke);
+    DeleteObject(hPen_polyline);
+    DeleteObject(hFont_dmg);
 }
 extern void inj(void) { // inject
-    HANDLE TTSW10 = *(HANDLE*)TTSW10_ADDR;
-    DWORD TSW_event_count = *(DWORD*)TTSW10_EVENT_COUNT_ADDR;
+    HANDLE TTSW10 = get_h(TTSW10_ADDR);
+    DWORD TSW_event_count = get_p(TTSW10_EVENT_COUNT_ADDR);
     if (!TSW_event_count)
     {
         draw_dmg(TTSW10);
         overlay(TTSW10);
     }/*
-    HANDLE TSW_mBitmap_1 = *(HANDLE*)TTSW10_GAMEMAP_BITMAP_1_ADDR;
-    HANDLE TSW_mBitmap_2 = *(HANDLE*)TTSW10_GAMEMAP_BITMAP_2_ADDR;
-    HDC TSW_mBitmap_hDC = (HDC)TCanvas_GetHandle(TBitmap_GetCanvas((DWORD)TSW_mBitmap_1));
+    HANDLE TSW_mBitmap_1 = get_h(TTSW10_GAMEMAP_BITMAP_1_ADDR);
+    HANDLE TSW_mBitmap_2 = get_h(TTSW10_GAMEMAP_BITMAP_2_ADDR);
+    HDC TSW_mBitmap_hDC = TCanvas_GetHandle(TBitmap_GetCanvas(TSW_mBitmap_1));
     draw_dmg(TTSW10, TSW_mBitmap_hDC);
-    TSW_mBitmap_hDC = (HDC)TCanvas_GetHandle(TBitmap_GetCanvas((DWORD)TSW_mBitmap_2));
+    TSW_mBitmap_hDC = TCanvas_GetHandle(TBitmap_GetCanvas(TSW_mBitmap_2));
     draw_dmg(TTSW10, TSW_mBitmap_hDC);*/
 }
