Z-H-Sun/tswKai

commit 4a8865c13a713bd7cbea5729abc55201c8912131

tswMP: bug fixes
- in sub_dmp @ tswMPExt1.asm: incorrect cmp when next_i-cur_i=+/-11 (Line 199-201)
- drawConnectivityOnBitmap: need to call `dtl` for `cur_i`, because if the player is on a tile that has a damage,
  the overlay, if present (the judgement is implemented in the new subroutine at the end of tswMPExt_1.asm),
  will be erased by TSW's redrawing
- connectivity highlight treatment in tswMPExt.rb and tswMPExt1.asm: was buggy when $MPshowMapDmg is off
  in the new mode: polyline and highlight won't be drawn onto game map bitmaps and will soon disappear;
  therefore, additional treatments were added to `always_show_overlay` (use value of -1 to indicate such cases)
  affected functions:
  - MPExt module functions @ tswMPExt.rb
  - sub_dmp @ tswMPExt_1.asm
  - sub_check_need_overlay @ tswMPExt_2.asm
  - newly added subroutine at the end of tswMPExt_1.asm (see also previous point)
- these changes are also reflected in tswMPExt_demo.c; dmg.dll.c; dmg.exe.c
  - also: added patch.h to handle common codes in tswMPExt_demo.c and dmg.exe.c
  - [!]TODO: tswMPDat.rb will be modified in the next commit
- tswMP.rb and common.rb: when disposing of DC, use SelectObject to put back stock pen or bitmap objects
  this is not necessary as discussed in the link below, but just to be extra cautious to avoid GDI leak
  https://github.com/users/Z-H-Sun/projects/2?pane=issue&itemId=55490542

common.rb
@@ -287,7 +287,10 @@ def disposeRes() # when switching to a new TSW process, hDC and hPrc will be reg
   end
   HookProcAPI.unhookK
   HookProcAPI.abandon(true)
-  DeleteObject.call($hBMP || 0)
+  if $hBMP
+    SelectObject.call($hMemDC, $hBMP0 || 0) # might be an overkill, but just to guarantee no GDI leak
+    DeleteObject.call($hBMP)
+  end
   DeleteDC.call($hMemDC || 0)
   VirtualFreeEx.call($hPrc || 0, $lpNewAddr || 0, 0, MEM_RELEASE)
   CloseHandle.call($hPrc || 0)

main.rbw
@@ -34,7 +34,7 @@ def init()
   hDC = GetDC.call_r($hWnd)
   $hMemDC = CreateCompatibleDC.call_r(hDC)
   $hBMP = CreateCompatibleBitmap.call_r(hDC, 40, 40)
-  SelectObject.call_r($hMemDC, $hBMP)
+  $hBMP0 = SelectObject.call_r($hMemDC, $hBMP)
   ReleaseDC.call($hWnd, hDC)
 
   $lpNewAddr = VirtualAllocEx.call_r($hPrc, 0, 4096, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE) # 1 page size

tswMP.rb
@@ -31,6 +31,7 @@ SetROP2 = API.new('SetROP2', 'LI', 'I', 'gdi32')
 
 HC_ACTION = 0
 DC_BRUSH = 18
+DC_PEN = 19
 DT_CENTER = 1
 DT_VCENTER = 4
 DT_SINGLELINE = 0x20
@@ -76,6 +77,7 @@ $x_pos = $y_pos = -1
 $hGUIFont = CreateFontIndirect.call_r(DAMAGE_DISPLAY_FONT.pack(LOGFONT_STRUCT))
 $hSysFont = GetStockObject.call_r(SYSTEM_FONT)
 $hBr = GetStockObject.call_r(DC_BRUSH)
+$hPen0 = GetStockObject.call_r(DC_PEN)
 $hPen = CreatePen.call_r(0, 3, HIGHLIGHT_COLOR[4])
 $hPen2 = CreatePen.call_r(0, 3, HIGHLIGHT_COLOR[-2])
 
@@ -167,6 +169,7 @@ module HookProcAPI
   def disposeHDC()
     @winDown = false
     return unless $hDC
+    SelectObject.call($hDC, $hPen0) # might be an overkill, but just to guarantee no GDI leak
     ReleaseDC.call($hWnd, $hDC)
     $hDC = nil # hDC already released; no longer valid
   end

tswMPExt.rb
@@ -17,8 +17,8 @@ module MPExt
     MP_PATCH_BYTES_1.each {|i| WriteProcessMemory.call_r($hPrc, i[0], i[2], i[1], 0)}
 
     callFunc(@_sub_fin) # this is just to guarantee no GDI leak, in case the previous run of tswKai3 failed to clean up on exit
-    if $MPshowMapDmg and $MPnewMode
-      WriteProcessMemory.call_r($hPrc, @_always_show_overlay, $MPshowMapDmg == 1 ? "\1" : "\0", 1, 0)
+    if $MPnewMode
+      changeState()
       callFunc(@_sub_ini)
     else
       WriteProcessMemory.call_r($hPrc, @_tswMP_overlay_enabled, "\0", 1, 0)
@@ -27,16 +27,9 @@ module MPExt
     MP_PATCH_BYTES_2.each {|i| WriteProcessMemory.call_r($hPrc, i[0], i[2], i[1], 0)}
   end
   def changeState
-    if !$MPshowMapDmg # s=1 (checked) --> s=0 (unchecked)
-      WriteProcessMemory.call_r($hPrc, @_always_show_overlay, "\0", 1, 0)
-      callFunc(@_sub_res)
-    elsif $MPshowMapDmg == 1 # s=2 (intermediate) --> s=1 (checked)
-      WriteProcessMemory.call_r($hPrc, @_always_show_overlay, "\1", 1, 0)
-    else # s=0 (unchecked) --> s=2 (intermediate)
-      callFunc(@_sub_ini)
-    end
+    WriteProcessMemory.call_r($hPrc, @_always_show_overlay, $MPshowMapDmg ? ($MPshowMapDmg == 1 ? "\1" : "\0") : "\xFF", 1, 0)
   end
   def finalize
-    callFunc(@_sub_res) if $MPshowMapDmg and ($MPnewMode == true) # do nothing if $MPnewMode==1
+    callFunc(@_sub_res) if ($MPnewMode == true) # do nothing if $MPnewMode==1
   end
 end

tswMPExt/dmg.dll.c
@@ -2,7 +2,7 @@
 // Likewise, no need to align stack on 16-byte boundaries (because this is not Linux; Windows use 4-byte boundaries instead), so added the 4-th "-m..." option: https://stackoverflow.com/a/43597693/11979352
 // Likewise, no need for procedure prolog and epilog (push ebp; mov ebp, esp; ...; leave), so added the following "-f..." option: https://stackoverflow.com/a/21620940/11979352
 // You MUST use a 32-bit compiler because only 32-bit DLL can be loaded by 32-bit TSW exe
-// gcc -std=gnu99 -Os -s -DNDEBUG -shared -Wl,--enable-auto-image-base,--enable-auto-import -mpush-args -mno-accumulate-outgoing-args -mno-stack-arg-probe -mpreferred-stack-boundary=2 -fomit-frame-pointer dmg.dll.def dmg.dll.c -o dmg.dll
+// gcc -std=gnu99 -Os -g -shared -Wl,--enable-auto-image-base,--enable-auto-import -mpush-args -mno-accumulate-outgoing-args -mno-stack-arg-probe -mpreferred-stack-boundary=2 -fomit-frame-pointer dmg.dll.def dmg.dll.c -o dmg.dll
 
 #include "TSW_memory.h"
 #define NOINLINE __attribute__((noinline))
@@ -66,7 +66,7 @@ BYTE *const p_need_update = BYTE_NEED_UPDATE_ADDR; /* the initial value is 1|2
     2 | do not update until all events are over (to prevent TSW's redrawing from erasing our drawing)
     3 | do not show dmg overlay until all events are over (useful when gameover)
     4 | restored original game map bitmaps or not */
-BYTE *const p_always_show_overlay = BYTE_ALWAYS_SHOW_OVERLAY_ADDR; // whether to always show dmg overlay, even without OrbOfHero
+char *const p_always_show_overlay = BYTE_ALWAYS_SHOW_OVERLAY_ADDR; // whether to always show/hide dmg overlay, regardless of OrbOfHero; 1=always show; -1=always hide; 0=depending on OrbOfHero
 HDC *const p_hMemDC = HMEMDC_ADDR;
 HBITMAP *const p_hMemBmp = HMEMBMP_1_ADDR; // 2 HBITMAPs; 2 game map bitmaps because there are 2 frames
 HPEN *const p_hPen_stroke = HPEN_STROKE_ADDR, *p_hPen_polyline = HPEN_POLYLINE_ADDR;
@@ -76,6 +76,7 @@ HFONT *const p_hFont_dmg = HFONT_DMG_ADDR;
 #define m_dmg_cri p_m_dmg_cri
 #define dll_init (*p_dll_init)
 #define need_update (*p_need_update)
+#define always_show_overlay (*p_always_show_overlay)
 #define hMemDC (*p_hMemDC)
 #define hMemBmp p_hMemBmp
 #define hPen_stroke (*p_hPen_stroke)
@@ -139,7 +140,7 @@ NOINLINE static void REGCALL drawConnectivityOnDC(const HDC hDC) { // draw polyl
     drawPolylineOnDC(hDC);
 }
 
-NOINLINE static HDC REGCALL drawConnectivityOnBitmap(const HANDLE TSW_cur_mBitmap) { // draw polyline and highlight on TTSW10_GAMEMAP_BITMAP_i
+NOINLINE static HDC REGCALL drawConnectivityOnBitmap(const HANDLE TSW_cur_mBitmap, const UCHAR TSW_tileSize) { // draw polyline and highlight on TTSW10_GAMEMAP_BITMAP_i
     const BYTE TSW_cur_frame = (BYTE)get_p(TTSW10_GAMEMAP_FRAME_ADDR);
     const BYTE test_bit = (TSW_cur_frame + 1 << 1); // (i+1)-th (i=0/1) bit
     const HDC TSW_cur_mBitmap_hDC = TCanvas_GetHandle(TBitmap_GetCanvas(TSW_cur_mBitmap));
@@ -159,6 +160,15 @@ NOINLINE static HDC REGCALL drawConnectivityOnBitmap(const HANDLE TSW_cur_mBitma
     SQUARE s; getHighlightSquare(&s);
     SelectObject(hMemDC, hMemBmp[TSW_cur_frame]);
     BitBlt(hMemDC, 0, 440, s.w, s.h, TSW_cur_mBitmap_hDC, s.x, s.y, SRCCOPY); // backup the tile cell that will be highlighted at (0, 440), which is definitely outside the map area
+
+    if ((always_show_overlay < 0) ||
+        ((always_show_overlay != 0) && (((ITEM*)TTSW10_HERO_ITEM_ADDR)->orbHero != 1))) {
+        const STATUS TSW_hero_status = get_t(TTSW10_HERO_STATUS_ADDR, STATUS);
+        const UCHAR cur_ix = TSW_hero_status.x, cur_iy = TSW_hero_status.y, cur_i = cur_ix + cur_iy*11;
+        WORD x = cur_ix*TSW_tileSize, y = (cur_iy+1)*TSW_tileSize;
+        dtl(TSW_cur_mBitmap_hDC, cur_i, MAKELONG(x, y)); // if the player is currently on a tile that has a damage, then need to update the dmg overlay bitmap; otherwise, the dmg will be erased by TSW's own redrawing
+    }
+
     drawConnectivityOnDC(TSW_cur_mBitmap_hDC);
     return TSW_cur_mBitmap_hDC;
 }
@@ -463,13 +473,14 @@ extern void REGCALL dmp(const HANDLE TTSW10_TCanvas, const DWORD TSW_mapLeft, co
 
     ////////// for connectivity polyline function //////////
     if (polyline_state) {
-        TSW_mBitmap_hDC = drawConnectivityOnBitmap(TSW_cur_mBitmap);
+        TSW_mBitmap_hDC = drawConnectivityOnBitmap(TSW_cur_mBitmap, TSW_tileSize);
         goto draw;
     }
     ////////// ---------------------------------- //////////
 
     TSW_mBitmap_hDC = TCanvas_GetHandle(TBitmap_GetCanvas(TSW_cur_mBitmap));
-    if (!(*p_always_show_overlay) && (((ITEM*)TTSW10_HERO_ITEM_ADDR)->orbHero != 1)) // unless (*p_always_show_overlay) is set, show no overlay if without OrbOfHero
+    if ((always_show_overlay < 0) ||
+        ((always_show_overlay != 0) && (((ITEM*)TTSW10_HERO_ITEM_ADDR)->orbHero != 1)))
         goto no_overlay;
     if (need_update & 8) { // if 3rd bit is set, show no overlay until event is over
         if ((int)get_p(TTSW10_EVENT_COUNT_ADDR) > 0) {

tswMPExt/dmg.exe.c
@@ -1,11 +1,6 @@
-// gcc -mwindows -std=gnu99 -Os -s -DNDEBUG -mpush-args -mno-accumulate-outgoing-args -mno-stack-arg-probe -mpreferred-stack-boundary=2 -fomit-frame-pointer dmg.exe.c -o dmg.exe
+// gcc -mwindows -std=gnu99 -Os -s -DNDEBUG dmg.exe.c -o dmg.exe
 
-#include <windows.h>
-typedef struct {
-    SIZE_T addr;
-    SIZE_T len;
-    const char* bytes;
-} PATCH;
+#include "patch.h"
 const PATCH patches[] = { /*
 4012B0:
   Kernel32_GetModuleHandleA:
@@ -382,11 +377,12 @@ sub_backup_tile_TCustomImageList_Draw:
 442F28:
 sub_check_need_overlay:	// when `tswMP_overlay_enabled`, `ret` to continue normal operation either if `always_show_overlay` or `TTSW10_HERO_ORB_OF_HERO_ADDR`; otherwise, stop the current caller function and `ret` to caller's caller
 	cmp byte ptr [DLL_IsInit], 0
-	je +12	// loc_check_need_overlay_false
+	je +14	// loc_check_need_overlay_false
 	cmp byte ptr [always_show_overlay], 0
-	jne +0C	//loc_check_need_overlay_true
+  js +0B	// loc_check_need_overlay_false
+	jne +0C	// loc_check_need_overlay_true
 	cmp byte ptr [TTSW10_HERO_ORB_OF_HERO_ADDR], 1
-	je +03	//loc_check_need_overlay_true
+	je +03	// loc_check_need_overlay_true
 
 	loc_check_need_overlay_false:	// `ret` to caller's caller (stop the current caller function)
 	add esp, 4	// pop the return address; do not execute the remaining commands in the caller function and jump back to caller's caller
@@ -575,7 +571,7 @@ loc_TTSW10_monidou_finalize:
     {0x451939, 20, "\xC7\x04\x45\x4E\xC7\x48\x00\x00\x00\x00\x00\x80\x0D\xB6\xA1\x4B\x00\x03\x66\x90"},
     {0x44A54A, 25, "\xBA\x54\xC5\x48\x00\x8B\x02\x2D\xC6\x00\x00\x00\x89\x42\x08\x89\x42\x64\x80\x0D\xB6\xA1\x4B\x00\x04"},
     {0x449E66, 13, "\x31\xDB\x89\x1C\x4A\x80\x0D\xB6\xA1\x4B\x00\x08\x90"},
-    {0x442F1D, 42, "\x68\x61\x2F\x44\x00\xFF\x35\x3C\xA2\x4B\x00\x80\x3D\xB5\xA1\x4B\x00\x00\x74\x12\x80\x3D\xB7\xA1\x4B\x00\x00\x75\x0C\x80\x3D\xCC\x86\x4B\x00\x01\x74\x03\x83\xC4\x04\xC3"},
+    {0x442F1D, 44, "\x68\x61\x2F\x44\x00\xFF\x35\x3C\xA2\x4B\x00\x80\x3D\xB5\xA1\x4B\x00\x00\x74\x14\x80\x3D\xB7\xA1\x4B\x00\x00\x78\x0B\x75\x0C\x80\x3D\xCC\x86\x4B\x00\x01\x74\x03\x83\xC4\x04\xC3"},
     {0x45458F, 164, "\x68\x10\x46\x45\x00\xA1\x70\xC5\x48\x00\x8A\x80\x1F\x9B\x48\x00\x50\xBE\xA0\x86\x4B\x00\x6B\x56\x04\x0B\x03\x16\x39\xD0\x75\x07\x8B\xC3\xE8\xEE\xED\xFE\xFF\xBA\x4C\xC5\x48\x00\x8B\x42\x30\x03\x42\x04\x8B\x4A\x2C\x03\x0A\x8B\x93\x54\x02\x00\x00\x8B\x52\x2C\x01\xC2\x66\x52\x66\x51\x50\x8B\x44\x24\x08\x6B\x56\xF8\x7B\x8A\x84\x10\x36\x89\x4B\x00\x48\x50\x8B\x93\x20\x01\x00\x00\x8B\xF2\x8B\x83\xB0\x01\x00\x00\xE8\x4A\x38\xFC\xFF\x8B\xC6\xE8\x4F\x63\xFC\xFF\x59\x5A\xE8\x20\xE9\xFE\xFF\xFF\x25\x40\xA2\x4B\x00\xC3\x90\xB9\xAC\xC5\x48\x00\x6B\x01\x06\x80\x7C\x08\x9A\x15\x0F\x85\x91\x01\x00\x00\x80\x7C\x08\x9C\x01\x0F\x85\x86\x01\x00\x00\xFF\x09\x8B\x01\x90"},
     {0x454741, 7, "\xE8\x4E\xFE\xFF\xFF\xEB\x6C"},
     {0x48074B, 49, "\xA1\x14\xC5\x48\x00\x0F\x45\x05\x18\xC5\x48\x00\xE8\x7C\xD3\xF9\xFF\x50\xFF\x35\x1C\xC5\x48\x00\x8B\x15\x4C\xC5\x48\x00\x8B\x0D\x50\xC5\x48\x00\xE8\x44\x9E\xF9\xFF\x58\xE8\x66\x00\x00\x00\xEB\x15"},
@@ -583,22 +579,7 @@ loc_TTSW10_monidou_finalize:
     {0x480A29, 95, "\xA1\x18\xC5\x48\x00\xE8\xA5\xD0\xF9\xFF\x50\xFF\x35\x1C\xC5\x48\x00\x8B\x15\x4C\xC5\x48\x00\x8B\x0D\x50\xC5\x48\x00\xE8\x6D\x9B\xF9\xFF\x58\xE8\x8F\xFD\xFF\xFF\x66\x90\x6B\x17\x06\x0F\xB7\x7C\x32\x02\x0F\xB7\x74\x32\x04\xBA\x00\x9C\x48\x00\x8B\x04\xBA\xC7\x04\xBA\xFE\xFF\xFF\xFF\x89\x04\xB2\x6B\x05\x98\x86\x4B\x00\x7B\x05\x36\x89\x4B\x00\x8A\x0C\x38\xC6\x04\x38\x06\x88\x0C\x30"},
     };
 
-#define check(ret) if(!ret) {msg = msg_f; goto end;}
 int main() {
-    const char* msg = "TSW injection OK. F1/F9 = Show/Hide on-map damage.";
-    const char* msg_f = "TSW injection failed, possibly because TSW is not running or you don't have proper permissions.";
-    HWND hWnd = FindWindow("TTSW10", NULL);
-    check(hWnd);
-    DWORD pID;
-    HANDLE hPrc;
-    GetWindowThreadProcessId(hWnd, &pID);
-    check(pID);
-    hPrc = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, 0, pID);
-    check(hPrc);
-    for(const PATCH *p = patches; p < (&patches)[1]; ++p)
-        check(WriteProcessMemory(hPrc, (LPVOID)p->addr, p->bytes, p->len, NULL));
-    CloseHandle(hPrc);
-end:
-    MessageBoxA(hWnd, msg, "TSW dmg extension", MB_ICONEXCLAMATION | MB_TOPMOST | MB_SETFOREGROUND);
+    patch(patches, (&patches)[1]);
     return 0;
 }

tswMPExt/patch.h
@@ -1,0 +1,25 @@
+#include <windows.h>
+typedef struct {
+    SIZE_T addr;
+    SIZE_T len;
+    const char* bytes;
+} PATCH;
+
+#define check(ret) if(!ret) {msg = msg_f; goto end;}
+static inline void patch(const PATCH *p_start, const PATCH *p_end) {
+    const char* msg = "TSW injection OK. F1/F9 = Show/Hide on-map damage.";
+    const char* msg_f = "TSW injection failed, possibly because TSW is not running or you don't have proper permissions.";
+    HWND hWnd = FindWindow("TTSW10", NULL);
+    check(hWnd);
+    DWORD pID;
+    HANDLE hPrc;
+    GetWindowThreadProcessId(hWnd, &pID);
+    check(pID);
+    hPrc = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, 0, pID);
+    check(hPrc);
+    for(const PATCH *p = p_start; p < p_end; ++p)
+        check(WriteProcessMemory(hPrc, (LPVOID)p->addr, p->bytes, p->len, NULL));
+    CloseHandle(hPrc);
+end:
+    MessageBoxA(hWnd, msg, "TSW dmg extension", MB_ICONEXCLAMATION | MB_TOPMOST | MB_SETFOREGROUND);
+}

commit 3cff5a401fd23b12825262c026965d241912c4e2

Partially cherry-pick "tswMP: bug fixes"
This applies changes to the following files introduced
in commit 4a8865c13a713bd7cbea5729abc55201c8912131:

- common.rb
- main.rbw
- tswMP.rb

The changes include:
* when disposing of DC, use SelectObject to put back
  stock pen or bitmap objects
  this is not necessary as discussed in the link below,
  but just to be extra cautious to avoid GDI leak
  https://github.com/users/Z-H-Sun/projects/2?pane=issue&itemId=55490542

common.rb
@@ -287,7 +287,10 @@ def disposeRes() # when switching to a new TSW process, hDC and hPrc will be reg
   end
   HookProcAPI.unhookK
   HookProcAPI.abandon(true)
-  DeleteObject.call($hBMP || 0)
+  if $hBMP
+    SelectObject.call($hMemDC, $hBMP0 || 0) # might be an overkill, but just to guarantee no GDI leak
+    DeleteObject.call($hBMP)
+  end
   DeleteDC.call($hMemDC || 0)
   VirtualFreeEx.call($hPrc || 0, $lpNewAddr || 0, 0, MEM_RELEASE)
   CloseHandle.call($hPrc || 0)

main.rbw
@@ -34,7 +34,7 @@ def init()
   hDC = GetDC.call_r($hWnd)
   $hMemDC = CreateCompatibleDC.call_r(hDC)
   $hBMP = CreateCompatibleBitmap.call_r(hDC, 40, 40)
-  SelectObject.call_r($hMemDC, $hBMP)
+  $hBMP0 = SelectObject.call_r($hMemDC, $hBMP)
   ReleaseDC.call($hWnd, hDC)
 
   $lpNewAddr = VirtualAllocEx.call_r($hPrc, 0, 4096, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE) # 1 page size

tswMP.rb
@@ -31,6 +31,7 @@ SetROP2 = API.new('SetROP2', 'LI', 'I', 'gdi32')
 
 HC_ACTION = 0
 DC_BRUSH = 18
+DC_PEN = 19
 DT_CENTER = 1
 DT_VCENTER = 4
 DT_SINGLELINE = 0x20
@@ -74,6 +75,7 @@ $x_pos = $y_pos = -1
 $hGUIFont = CreateFontIndirect.call_r(DAMAGE_DISPLAY_FONT.pack(LOGFONT_STRUCT))
 $hSysFont = GetStockObject.call_r(SYSTEM_FONT)
 $hBr = GetStockObject.call_r(DC_BRUSH)
+$hPen0 = GetStockObject.call_r(DC_PEN)
 $hPen = CreatePen.call_r(0, 3, HIGHLIGHT_COLOR[4])
 $hPen2 = CreatePen.call_r(0, 3, HIGHLIGHT_COLOR[-2])
 
@@ -165,6 +167,7 @@ module HookProcAPI
   def disposeHDC()
     @winDown = false
     return unless $hDC
+    SelectObject.call($hDC, $hPen0) # might be an overkill, but just to guarantee no GDI leak
     ReleaseDC.call($hWnd, $hDC)
     $hDC = nil # hDC already released; no longer valid
   end